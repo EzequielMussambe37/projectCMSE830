82da8d6c98966db2791b179d79085772081ce234
Ezequiel Mussambe <ezequielmussambe@Ezequiels-MacBook-Pro.local>
2023-10-02 21:58:42 -0400

    First representation of fututreApp

--- classApp.py
+++ classApp.py
@@ -1,25 +1,33 @@
 
 import streamlit as st
+import pandas as pd
 simple_plot_List = ["pairplot", "histgram","barplot","violinplot","boxplot"]
-'''
-    colorblind. importante escolher bem,
-    python tem  spetrum for colorblind...
-    como mudaram as colors no python libraries tipo seaborn, matplotlib,etc...
-'''
+
 advance_plot_list = []
-class mainApp(st):
+class mainApp(object):
 
-    def __init__(self,username,auth,):
-
-        self.username = username
-        self.auth = auth
-
+    def __init__(self):
+        self.page_title = "University Recommendation System"
+        self.page_icon = ":money_with_wings:"
+        self.layout = "centered" #centered
+        st.set_page_config(self.page_title,self.layout)
+        st.title(self.page_title)
+        self.df = object
 
     def dataVisusalization(self):
         #operation to show
-
+        pass
 
     def basicPlots(self,plotName):
+        #show basic plots
+        pass
+    
+    def read_dataframe(self,data):
+        self.df = pd.read_csv(data)
+        return self.df
 
-        if plotName
 
+if __name__ == '__main__':
+    data_file = "recommendation.csv"
+    app = mainApp()
+    app.read_dataframe(data_file)
