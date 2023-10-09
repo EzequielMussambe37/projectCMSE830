013c5b89943213738d8e6920b15b08e18918542f
Ezequiel Mussambe <47234997+EzequielMussambe@users.noreply.github.com>
2023-10-08 22:57:19 -0400

    Created Plots options for the project

--- utilities/EDA.py
+++ utilities/EDA.py
@@ -1,40 +1,88 @@
 import streamlit as st
-from utilities import dataMani 
+from utilities import dataMani , plots
 import pandas as pd
 
 def app():
     st.title("Data Visualization Main")
     f= dataMani.upload_single_file()
+    st.markdown("""---""")
     features(f)
-    #st.dataframe(f)
-
-    #st.write_csv("chances.csv",uploaded.upload_url)
-    # df = st.dataframe(f)
-    # df
-    #features()
-
+    st.markdown("""---""")
 def features(data,column=[]):
     p = []
     df = object
+    plots_object = object
     with st.sidebar:
 
-        selection = st.selectbox (
-            "Visualize DataFrame",
-            ("", "View All", "Select Atributes")
+        selection = st.radio (
+            "Data Table",
+            ["None", "All"],
+            horizontal=True
         )
+        st.markdown("""___""")
+        st.title("Plots")
+        plots_object = plot_names()
 
-    if selection == "Select Atributes":
-        with st.sidebar:
-                options = st.multiselect(
-                    "What Features",
-                    dataMani.read_data(data,[]).columns.tolist()
-                )
-        if len(options)>0:
-            df = dataMani.read_data(data,options).transpose()
+    if selection == "All":
+        with st.expander("See the table"):
+            df = dataMani.read_data(data,[])
             st.dataframe(df)
-    elif selection == "View All":
-        df = dataMani.read_data(data,[])
-        st.dataframe(df)
-        
+    for key, value in plots_object.items():
+        if key == "barplot":
+            if value == True:
+                x = xy_dataframe(data,key)
+                #st.plotly_chart(plots.plotly_group(x,x.columns.tolist()), use_container_width=True)
+        if key == "boxplot":
+            if value == True:
+                x = xy_dataframe(data,key)
+                print(x[0])
+                plots.seanborn_plot(x[0])
+                #st.plotly_chart(plots.seanborn_plot(x),use_container_width=True)
+
+   
     return df
 
+def plot_names():
+    names = ["barplot","boxplot","matplot","l"]
+    ss= {}
+    column1,column2 = st.columns(2)
+    for index, name in enumerate(names,1):
+        if index%2==0:
+            with column1:
+                n = st.checkbox(name)
+                ss[name] = n
+        else:
+            with column2:
+                n = st.checkbox(name)
+                ss[name] = n
+    return ss
+
+
+def xy_dataframe(data,key):
+    
+    column1, column2 = st.columns([0.7, 0.3],gap="small")
+    if key in ["barplot","boxplot"]:
+        column1, column2 = st.columns([0.9, 0.1],gap="small")
+    x_frame = []
+    y_target = []
+    with column1:
+        x_variables = st.multiselect(
+            "X Features",
+            dataMani.read_data(data,[]).columns.tolist(),
+            key=key
+        )
+        if len(x_variables)>0:
+            x_frame = dataMani.read_data(data,x_variables)
+    if key not in ["barplot","boxplot"]:
+        with column2:
+            target = st.selectbox(
+                "Target",
+
+                tuple([" "]+ dataMani.read_data(data,[]).columns.tolist()),
+                    help="Insert a column that can be used as target when plot xy graph",
+                    key=key+'1'
+            )
+
+        if target != " ":
+            y_target = dataMani.read_data(data,target)
+    return [x_frame,y_target]
--- utilities/dataMani.py
+++ utilities/dataMani.py
@@ -14,12 +14,47 @@ def read_data(df,columns):
     
     
 def upload_single_file():
-    uploaded = st.file_uploader("Please Choose a csv file",type={"csv", "txt"})
-    print(uploaded.name)
     
-    return read_file(uploaded)
-    # else:
-    #     st.warning("you need to upload csv file")
-def read_file(file):
-    f = pd.read_csv(file)
+    
+    options = upload_load_ExistedFile()
+    print(options)
+    if options =="Load Existed File":
+        return read_file("csv",",","chances.csv")
+
+    elif options == "Upload a new File":
+        colum1,colum2 = st.columns(2)
+
+        text_delimeter = {"comma":",","whitespace":" ","semi-colon":";"}
+        with colum1:
+            delimiter = st.selectbox(
+                "Text Delimiter",
+                tuple(list(text_delimeter.keys()))
+            )
+
+        with colum2:
+            uploaded = st.file_uploader("Please Choose a csv file")
+        extension = uploaded.name.split(".")[1].strip().lower()
+       
+        return read_file(extension,text_delimeter[delimiter],uploaded)
+
+def read_file(extension,delimiter, file):
+
+    if extension == "csv":
+        f = pd.read_csv(file,delimiter=delimiter)
+    elif extension == "txt":
+        f = pd.read_table(file,delimiter=delimiter)
+    elif extension == "xlsx":
+        f = pd.read_excel(file,delimiter=delimiter) 
+    f.drop(f.columns[f.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
     return f
+
+
+def upload_load_ExistedFile():
+    options= st.radio( 
+        "Choose Option to Load Dataset",
+        ["Load Existed File","Upload a new File"],
+        horizontal=True
+    )
+
+    return options
+# css-1erivf3 e1b2p2ww15
