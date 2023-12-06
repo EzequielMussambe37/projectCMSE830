10b249dea1c0d4e5f6ac327cfdbb22f8b1b81e5b
Ezequiel Mussambe <ezequielmussambe@Ezequiels-MacBook-Pro.local>
2023-12-06 10:33:41 -0500

    Introduction Refined

--- utilities/conclusion.py
+++ utilities/conclusion.py
@@ -4,8 +4,23 @@ from utilities import projectPlots as pp, read_data,EDA
 
 
 def app():
-    st.header("Conclusion")
-#     st.markdown("___")
+        st.header("Conclusion")
+        st.markdown("""
+                In summary, the linear regression model in this app by default predicted graduate admission chances based on 
+                CGPA, GRE, and TOEFL scores. With an R-squared value of .79, the model reliably explains 79% of admission chance variations. 
+                This application offers valuable insights into predicting graduate admission outcomes.
+                """)
+
+        st.markdown("""
+                While the model offers valuable insights, it has limitations, such as assuming linearity and potential unobserved variables. 
+                Predictive power may vary among institutions or programs, requiring caution in generalizing results.
+                This research adds to graduate admissions knowledge through a quantitative approach. 
+                        """)
+        st.markdown("""
+                        Future studies can delve into additional features, nonlinear relationships or focus on specific academic programs for more refined predictive model.
+                Universities and applicants alike can gain from understanding the contributing variables.
+
+                        """)
 #     data=EDA.load_data()
 
 #     fig = pp.plotly_scatter(data,"CGPA", "Chance of Admit ","Chance of Admit ")
